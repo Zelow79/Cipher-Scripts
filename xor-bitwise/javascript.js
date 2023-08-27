1644b7295341d0b23ccbac4ee160c1d0d9967aea
Zelow79 <zelow79@gmail.com>
2023-08-27 05:06:33 -0400

    Cipher 1.0.0
    Inputting text into the text box ciphers it in real time.

---
+++ xor-bitwise/javascript.js
@@ -0,0 +1,19 @@
+function cipher() {
+    const code = [420], //array of secret codes for encryption
+        word = document.getElementById("input").value;
+	if (!word) return null;
+
+	function process(w, c) {
+		const encrypted = []; //storage for encryption
+		for (let i = 0; i < w.length; i++) { //iterate word letter by letter
+			encrypted.push(w.charCodeAt(i) ^ c); //convert letter to char number and xor bit shift by code
+		}
+		return String.fromCharCode(...encrypted) //return converted char numbers back to string
+	}
+	let result = word; //result will be modified based on number of codes given in code array
+	for (let i = 0; i < code.length; i++) {
+		result = process(result, code[i]);
+	}
+    document.getElementById("output").innerHTML = result;
+	//return result;
+}
